368577b5c53cc945d5135737d58e28230baf91e6
Aixiu AN <44171416+aixiuxiuxiu@users.noreply.github.com>
2019-08-19 17:46:53 +0200

    Add files via upload

---
+++ Experiments/inverted_coordination/fr/expand_items_num_inv.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+import pandas as pd
+
+conditions = {
+    'Vsg-Nsg-and': ['prefix', 'Vsg', 'det1sg', 'np1sg','and'],
+    'Vpl-Nsg-and': ['prefix', 'Vpl', 'det1sg', 'np1sg','and'],
+    'Vsg-Npl-and': ['prefix', 'Vsg', 'det1pl', 'np1pl','and'],
+    'Vpl-Npl-and': ['prefix', 'Vpl', 'det1pl', 'np1pl','and'],
+    'Vsg-Nsg-or': ['prefix', 'Vsg', 'det1sg', 'np1sg','or'],
+    'Vpl-Nsg-or': ['prefix', 'Vpl', 'det1sg', 'np1sg','or'],
+    'Vsg-Npl-or': ['prefix', 'Vsg', 'det1pl', 'np1pl','or'],
+    'Vpl-Npl-or': ['prefix', 'Vpl', 'det1pl', 'np1pl','or'],
+}
+end_condition_included = False
+autocaps = True
+
+def expand_items(df):
+    output_df = pd.DataFrame(rows(df))
+    output_df.columns = ['sent_index', 'word_index', 'word', 'region', 'condition']
+    return output_df
+
+def rows(df):
+    for condition in conditions:
+        for sent_index, row in df.iterrows():
+            word_index = 0
+            for region in conditions[condition]:
+                for word in row[region].split():
+                    if autocaps and word_index == 0:
+                        word = word.title()
+                    yield sent_index, word_index, word, region, condition
+                    word_index += 1
+            if not end_condition_included:
+                yield sent_index, word_index + 1, "<eos>",condition
+
+def main(filename, output_file):
+    filename=sys.argv[1]
+    output_file=sys.argv[2]
+    input_df = pd.read_excel(filename)
+    output_df = expand_items(input_df)
+    try:
+        output_df.to_csv("items_num_inv.txt", sep="\t")
+    except FileExistsError:
+        pass
+
+    
+if __name__ == "__main__":
+    main(*sys.argv[1:])
+
+
---
+++ Experiments/inverted_coordination/fr/input_num_inv.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 15:48:47 2019
+
+@author: aixiuan
+"""
+
+import os
+import sys
+import csv
+
+import pandas as pd
+
+
+conditions = {
+    'Vsg-Nsg-and': ['prefix', 'Vsg', 'det1sg', 'np1sg','and'],
+    'Vpl-Nsg-and': ['prefix', 'Vpl', 'det1sg', 'np1sg','and'],
+    'Vsg-Npl-and': ['prefix', 'Vsg', 'det1pl', 'np1pl','and'],
+    'Vpl-Npl-and': ['prefix', 'Vpl', 'det1pl', 'np1pl','and'],
+    'Vsg-Nsg-or': ['prefix', 'Vsg', 'det1sg', 'np1sg','or'],
+    'Vpl-Nsg-or': ['prefix', 'Vpl', 'det1sg', 'np1sg','or'],
+    'Vsg-Npl-or': ['prefix', 'Vsg', 'det1pl', 'np1pl','or'],
+    'Vpl-Npl-or': ['prefix', 'Vpl', 'det1pl', 'np1pl','or'],
+}
+
+end_condition_included = False
+autocaps = True
+
+
+
+def rows(df):
+    sent=[]
+    for condition in conditions.keys():
+        sent.append(df[conditions[condition][0]].str.strip() +' '+df[conditions[condition][1]].str.strip()+ ' '+df[conditions[condition][2]].str.strip()+' '+df[conditions[condition][3]].str.strip()+' '+df[conditions[condition][4]].str.strip())
+    return sent
+
+
+def expand_items(df):
+    output_df = pd.DataFrame(rows(df))
+    return output_df
+
+def main(filename,outputfile):
+    filename=sys.argv[1]
+    outputfile=sys.argv[2]
+    input_df = pd.read_excel(filename)
+    output_df = expand_items(input_df)
+    try:
+        output_df.to_csv(outputfile, index= False, header= False, quoting=csv.QUOTE_NONE, quotechar='"',  sep='\n')
+    except FileExistsError:
+        pass
+
+
+
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
+
